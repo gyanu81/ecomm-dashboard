c9808dbea004064f886f5ce6fa87c26d58d42ec3
gyanu81 <gyanukumar009@gmail.com>
2024-09-25 18:57:56 +0530

    Add project files with corrected line endings

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button }from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+   <h1>
+    e-comm project 
+    <button> normal button</button>
+    <Button>bootstrap button</Button>
+   </h1>
+    </div>
+  );
+}
+
+export default App;
